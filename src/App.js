a868f52627971e8e9e71aa6f16ac5ff921e59d5e
leungcheryl <leung.cheryl@icloud.com>
2019-06-01 17:16:41 -0400

    functional first axios call

--- src/App.js
+++ src/App.js
@@ -7,31 +7,60 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-     
+      userInput: ''
     }
   }
 
-  componentDidMount() {
+  getMovie() {
+    const movieInput = this.state.userInput
+    console.log(movieInput)
 
-    axios.get('https://cors-anywhere.herokuapp.com/https://www.doesthedogdie.com/search?', {
-      method: 'GET',
-      dataType: 'json',
-      headers: {
-        "X-API-KEY": "e39ba046c39413e2c04848ae44e80a73",
-        "Accept": "application/json"
-      },
-      params: {
-        q: `marley`
-      }
-    }).then(response => {
-      console.log(response)
+      axios.get(
+          'https://cors-anywhere.herokuapp.com/https://www.doesthedogdie.com/search?',
+          {
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+              'X-API-KEY': 'e39ba046c39413e2c04848ae44e80a73',
+              Accept: 'application/json'
+            },
+            params: {
+              q: movieInput
+            }
+          }
+        )
+        .then(response => {
+          console.log(response)
+        })
+
+  } 
+
+  handleChange = (event) => {
+    this.setState({
+      userInput: event.target.value
     })
-  }s
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({
+      userInput: ''
+    })
+    this.getMovie()
+  }
 
   render() {
     return (
     <div className="App">
-      <input type='text' placeholder='Enter your search term' name='search' />
+      <form action="">
+        <input 
+          onChange={this.handleChange} 
+          type='text' 
+          placeholder='Type Movie Here' 
+          value={this.state.userInput}
+          name='search' />
+        <button onClick={this.handleClick}>Search!</button>
+      </form>
     </div>
     );
   }
